352eedec987b6cf8af061d45f49d419d25534303
GitHubermane <crykour12@gmail.com>
2022-08-15 16:39:28 +0300

    Initialization

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import { Header } from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="App">
+        Hi World
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FaceIcon from '@mui/icons-material/Face';
+import { IconButton } from '@mui/material';
+
+export const Header = () => {
+    return (
+        <div className='Header'>
+            <div className='Header__wrapper'>
+                <div className='Header__logo'>
+                    <FaceIcon />
+                </div>
+                <div>
+                    Header
+                </div>
+                <IconButton className='Header__cartBtn'>
+                    <ShoppingCartIcon className='Header__cartIcon'/>
+                </IconButton>
+            </div>
+        </div>
+    )
+}
